f9b1b2267c2b677b058dbc1089fa130ba2426d8d
lovely fernandez <lovelyfernandez1402@outlook.com>
2025-05-04 22:59:20 +0100

    sfasdfa

--- fetch/cloud.gd
+++ fetch/cloud.gd
@@ -1,15 +1,19 @@
 extends Node3D
 
-@export var wind_direction: Vector3 = Vector3(1, 0, 0)  
-@export var wind_speed: float = 2.0                    
-@export var cloud_reset_distance: float = 100.0         
-@export var cloud_spawn_offset: float = -100.0         
+@export var wind_direction: Vector3 = Vector3(1, 0, 0)
+@export var wind_speed: float = 1.0
+@export var cloud_reset_distance: float = 100.0
+@export var cloud_spawn_offset: float = -100.0
 
 func _physics_process(delta):
+	var dir = wind_direction.normalized()
 	for cloud in get_children():
 		if cloud is Node3D:
-			cloud.translate(wind_direction.normalized() * wind_speed * delta)
+			cloud.translate(dir * wind_speed * delta)
 
-			var relative_pos = global_transform.origin - cloud.global_transform.origin
-			if relative_pos.length() > cloud_reset_distance:
-				cloud.translate(wind_direction.normalized() * cloud_spawn_offset)
+			# Check how far the cloud has moved along the wind direction
+			var local_offset = cloud.global_transform.origin - global_transform.origin
+			var projected = local_offset.dot(dir)
+
+			if projected > cloud_reset_distance:
+				cloud.translate(dir * cloud_spawn_offset)
